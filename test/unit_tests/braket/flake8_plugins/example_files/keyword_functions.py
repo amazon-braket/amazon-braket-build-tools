6b5aa983c79c9c5dfdb05a068cc7eed98915fafe
Milan Krneta <krneta@amazon.com>
2023-05-26 12:38:34 -0700

    adding failure cases for keywords

---
+++ test/unit_tests/braket/flake8_plugins/example_files/keyword_functions.py
@@ -0,0 +1,10 @@
+def function_0(arg0: str, *, arg1: int = 0, arg2: int = 1, arg3: int = 1) -> int:
+    """This is a description.
+    Args:
+        arg0 (str): This is a parameter
+        arg2 (bool): This is another parameter
+        arg3: This is yet another parameter
+    Returns:
+        int: value of the function.
+    """
+    pass
